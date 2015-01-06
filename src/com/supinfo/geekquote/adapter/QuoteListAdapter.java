a6795ea63b9bfb25314709b0186af7f65c97660b
NGUYEN Alexandre <alex.nr@hotmail.fr>
2015-01-06 16:11:51 +0100

    can edit and rate

---
+++ src/com/supinfo/geekquote/adapter/QuoteListAdapter.java
@@ -0,0 +1,64 @@
+package com.supinfo.geekquote.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+import com.supinfo.geekquote.model.Quote;
+
+import java.util.List;
+
+public class QuoteListAdapter extends BaseAdapter {
+
+    private List<Quote> quotes;
+    private LayoutInflater inflater;
+    private int resource;
+    private Context context;
+
+    public QuoteListAdapter(Context context, int textViewResourceId, List<Quote> quotes) {
+        this.quotes = quotes;
+        this.inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        this.resource = textViewResourceId;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return quotes.size();
+    }
+
+    @Override
+    public Quote getItem(int i) {
+        return quotes.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View convertView, ViewGroup parentView) {
+        View view;
+        TextView textView;
+
+        if (convertView == null) {
+            view = inflater.inflate(resource, parentView, false);
+        } else {
+            view = convertView;
+        }
+        textView = (TextView) view;
+        Quote quote = getItem(i);
+        textView.setText(quote.getStrQuote());
+        if (i % 2 == 0) {
+            textView.setBackgroundColor(context.getResources().getColor(android.R.color.background_dark));
+            textView.setTextColor(context.getResources().getColor(android.R.color.white));
+        } else {
+            textView.setBackgroundColor(context.getResources().getColor(android.R.color.background_light));
+            textView.setTextColor(context.getResources().getColor(android.R.color.black));
+        }
+        return textView;
+    }
+}
--- src/com/supinfo/geekquote/model/Quote.java
+++ src/com/supinfo/geekquote/model/Quote.java
@@ -1,22 +1,20 @@
 package com.supinfo.geekquote.model;
 
+import java.io.Serializable;
 import java.util.Date;
 
-/**
- * Created by alexandrenguyen on 06/01/15.
- */
-public class Quote {
+public class Quote implements Serializable {
 
     private String strQuote;
     private int rating;
     private Date creationDate;
 
-    public Date getCreationDate() {
-        return creationDate;
+    public String getStrQuote() {
+        return strQuote;
     }
 
-    public void setCreationDate(Date creationDate) {
-        this.creationDate = creationDate;
+    public void setStrQuote(String strQuote) {
+        this.strQuote = strQuote;
     }
 
     public int getRating() {
@@ -27,20 +25,42 @@ public class Quote {
         this.rating = rating;
     }
 
-    public String getStrQuote() {
-        return strQuote;
+    public Date getCreationDate() {
+        return creationDate;
     }
 
-    public void setStrQuote(String strQuote) {
-        this.strQuote = strQuote;
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Quote quote = (Quote) o;
+
+        if (rating != quote.rating) return false;
+        if (creationDate != null ? !creationDate.equals(quote.creationDate) : quote.creationDate != null) return false;
+        if (strQuote != null ? !strQuote.equals(quote.strQuote) : quote.strQuote != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = strQuote != null ? strQuote.hashCode() : 0;
+        result = 31 * result + rating;
+        result = 31 * result + (creationDate != null ? creationDate.hashCode() : 0);
+        return result;
     }
 
     @Override
     public String toString() {
         return "Quote{" +
-                "creationDate=" + creationDate +
-                ", strQuote='" + strQuote + '\'' +
+                "strQuote='" + strQuote + '\'' +
                 ", rating=" + rating +
+                ", creationDate=" + creationDate +
                 '}';
     }
 }
